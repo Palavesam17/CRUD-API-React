33b8f3f928ec2237e6bb8cd26aa7ac00f49be8a7
Palavesam17 <148433606+Palavesam17@users.noreply.github.com>
2025-01-19 20:24:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CrudApps from "./CRUD/CrudApps";
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      < CrudApps />
+      
+      
+   </div>
+  );
+}
+export default App;
